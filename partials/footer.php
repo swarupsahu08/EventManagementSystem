62cae50573ee856dcdac2d8372804b93da833cea
Swarup Kumar sahu <104717861+swarupsahu08@users.noreply.github.com>
2023-06-05 14:29:37 +0530

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,3 @@
+<footer>
+	<p>&copy; 2023 Event Management System. All rights reserved.</p>
+</footer>
---
+++ partials/nav.php
@@ -0,0 +1,11 @@
+<nav>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="uevents.php">Events</a></li>
+		<li><a href="about.php">About Us</a></li>
+		<li><a href="contact.php">Contact Us</a></li>
+		<li><a href="login.php">Login</a></li>
+    	<li><a href="register.php">Register</a></li>
+		<li><a href="admin.php">Admin</a></li>
+	</ul>
+</nav>
