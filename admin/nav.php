b845f92a4e7447db695a9b7aeda28dfc76b9b19a
Swarup Kumar sahu <104717861+swarupsahu08@users.noreply.github.com>
2023-06-05 14:22:46 +0530

    Create navigation bar

---
+++ admin/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a href="events.php">Events</a></li>
+        <li><a href="create_events.php">Create Events</a></li>
+        <li><a href="manage_users.php">Manage Users</a></li>
+    </ul>
+</nav>
